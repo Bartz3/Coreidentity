572318714b7fc1f892541f36bb6f92b35a772666
Bartek Późniewski <bartek.pozniewski@wp.pl>
2022-05-16 15:19:57 +0200

    Add project files.

---
+++ Pages/Admin/adminarea.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CoreIdentity.Pages.Admin
+{
+    public class adminareaModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,44 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+//builder.Services.AddRazorPages();
+
+builder.Services.AddAuthentication("CookieAuthentication")
+ .AddCookie("CookieAuthentication", config =>
+ {
+     config.Cookie.HttpOnly = true;
+     config.Cookie.SecurePolicy = CookieSecurePolicy.None;
+     config.Cookie.Name = "UserLoginCookie";
+     config.LoginPath = "/Login/UserLogin";
+     config.Cookie.SameSite = SameSiteMode.Strict;
+ });
+
+builder.Services.AddRazorPages(options =>
+{
+    options.Conventions.AuthorizeFolder("/Admin");
+});
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseCookiePolicy();     //added
+app.UseAuthentication();   //added
+
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+app.Run();
